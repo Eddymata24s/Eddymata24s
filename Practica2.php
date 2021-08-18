fd2258c1755e17fa6034b17402a7e841b2e5b76b
Eddymata24s <88917850+Eddymata24s@users.noreply.github.com>
2021-08-18 18:27:57 -0500

    Add files via upload

---
+++ Practica2.php
@@ -0,0 +1,18 @@
+<html>
+<title>Practica2</title>
+<body>
+	<body gbcolor="#9C09C7" text=#000000>
+<center>
+	<?php 
+	echo "<font face = 'Times New Roman' size = '100' COLOR='red'>Cesar Eduardo Mata Hernandez </FONT>";
+	echo "<FONT face = 'Cambria' size = '80' COLOR='blue'><br>Universidad Tecnologica de Torreon </FONT>";
+	echo "<font face = 'Arial' size = '40' COLOR='pink'><br>Matricula 20170200 </FONT>";
+	echo "<font face = 'Optima' size = '10' COLOR='gray'><br>Grado y Seccion 3 TI DSM TN </FONT>";
+	echo "<font face = 'Optima' size = '10' COLOR='gray'><br>Un Among? </FONT>";
+	?>
+</center>
+<center>
+	<img src = "among.gif">
+</center>
+</body>
+</html>
